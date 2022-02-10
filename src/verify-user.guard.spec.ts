87424c14af1868286907694d4c19bbd1afa1654d
Vex-iqualify <vex@iqualify.com>
2022-02-11 09:50:23 +1300

    Create a guard using nest g guard

---
+++ src/verify-user.guard.spec.ts
@@ -0,0 +1,7 @@
+import { VerifyUserGuard } from './verify-user.guard';
+
+describe('VerifyUserGuard', () => {
+  it('should be defined', () => {
+    expect(new VerifyUserGuard()).toBeDefined();
+  });
+});
